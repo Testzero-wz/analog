a47118f71d4ba1aceb2264c67db1aa13b7dbe737
T3stzer0 <testzerowz@gmail.com>
2020-12-08 00:49:06 +0800

    analog 2.0
    1. add real-time logs update
    2. add docs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from analog.bin.controller.controller import Controller
-import os,sys
-
+import os, sys
+import traceback
 
 if sys.version_info < (3, 5):
     sys.stdout.write("Requires Python 3.5 or higher ")
@@ -8,15 +8,21 @@ if sys.version_info < (3, 5):
 
 
 def main():
+    controller = None
     try:
         path = os.path.dirname(os.path.realpath(__file__))
         controller = Controller(path)
         controller.mainloop()
-
     except KeyboardInterrupt:
         print("User abort")
     except Exception as e:
+        traceback.print_exc()
         print(e)
+    finally:
+        if controller is not None:
+            controller.update_thread_stop_flag = True
+            controller.output.print_info("Waiting for update thread exit...")
+            controller.update_thread.join()
 
 
 if __name__ == "__main__":
