274d19b8fffaa7c56f8fdab74ddaf03ba876ce8b
Testzero <15984988+Testzero-wz@users.noreply.github.com>
2021-04-03 21:44:32 +0800

    Update TfidfVector.py
    add train file check

--- analog/bin/machine_learning/TfidfVector.py
+++ analog/bin/machine_learning/TfidfVector.py
@@ -3,6 +3,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import os
 from analog.bin.lib.utils import read_by_group
 import string
+from analog.bin.exception.Exceptions import *
 
 
 class TfidfVector(TfidfVectorizer):
@@ -25,8 +26,12 @@ class TfidfVector(TfidfVectorizer):
         fit_list = []
         if os.path.isfile(self.log_path):
             read_by_group(self.log_path, fit_list, pattern=self.config.get(self.section_name_log, 'log_content_pattern'))
+            if len(fit_list) == 0:
+                raise FileEmptyError
             self.fit_vector = self.fit_transform(fit_list)
-
+        else:
+            raise FileNotFound
+            
     @staticmethod
     def vocabulary_iter():
         for i in string.printable:
